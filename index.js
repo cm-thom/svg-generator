215cbed91af84d6d11fd95f81e3a977a10e26194
Cole <122782745+cm-thom@users.noreply.github.com>
2023-06-13 20:25:53 -0400

    almost functional. only circle works, needs to abstract out shape creation numbers

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const saveSVG = (mySVG) => {
+    fs.writeFile('./examples/logo.svg', mySVG, (err) => {
+        console.log(err)
+    });
+};
+
 inquirer.prompt([
     {
         type: 'input',
@@ -27,7 +33,20 @@ inquirer.prompt([
     }
 ])
 
-//then take that obj and use data.whatever to fill in the calculation for the svg gen
+//take user data, insert as template literals to make text, shape, colors
 .then((data) => {
     console.log(data)
-})
+    const mySVG = `<svg width="100" height="100">
+  <${data.shape} cx="50" cy="50" r="40" fill="${data.shapeColor}" />
+
+<text x="100" y="75" font-size="40" text-anchor="middle" fill="${data.characterColor}">${data.characters}</text>
+
+  </svg>
+`
+//then save to a .svg in examples
+    saveSVG(mySVG);
+});
+
+
+
+
